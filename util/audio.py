0987cedd0d6a6909749c594ca978ac4e11ae79ae
Ryuichi Yamamoto <zryuichi@gmail.com>
2017-10-31 21:48:37 +0900

    Workaround for latest librosa

--- util/audio.py
+++ util/audio.py
@@ -5,7 +5,7 @@ import numpy as np
 import tensorflow as tf
 from scipy import signal
 from hparams import hparams
-
+from scipy.io import wavfile
 
 def load_wav(path):
   return librosa.core.load(path, sr=hparams.sample_rate)[0]
@@ -13,7 +13,7 @@ def load_wav(path):
 
 def save_wav(wav, path):
   wav *= 32767 / max(0.01, np.max(np.abs(wav)))
-  librosa.output.write_wav(path, wav.astype(np.int16), hparams.sample_rate)
+  wavfile.write(path, hparams.sample_rate, wav.astype(np.int16))
 
 
 def preemphasis(x):
